121efa430f47f2870aba7f661bc3928555fc478e
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-09-05 22:39:06 +0800

    add: ellipse and polygon

--- practice/pillowPrac.py
+++ practice/pillowPrac.py
@@ -3,19 +3,25 @@
 from PIL import Image, ImageDraw
 
 # then make variable to load the image, use open() method
-img = Image.open("assets/anime.jpg")
+#img = Image.open("assets/anime.jpg")
 
 # then show the image
-img.show()
+#img.show()
 
 # now, we will make canvas to use that , we will use new() method
 img = Image.new(mode="RGBA", size=(500,500), color=(157,221,200,300))
 
 # then, use Image.Draw() then the parameter is the new canvas
 draw = ImageDraw.Draw(img)
+
 # to draw rectangle you can call rectangle parameters is the x and y the outline, and fill
-draw.rectangle([100,200,150,250], outline="black", fill="white")
+#draw.rectangle([100,200,150,250], outline="black", fill="white")
+#img.show()
+
+# to draw polygon or triangle
+#draw.polygon([(200, 50), (200,100), (50,100)], outline="black", fill="white")
+#img.show()
+
+# to draw circle/ellipsis
+draw.ellipse([100, 200, 300, 400], outline="black", fill="white")
 img.show()
-
-
-
