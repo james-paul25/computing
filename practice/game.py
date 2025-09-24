4445c4f486fd3957a4ae021eec8658349403ac00
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-09-24 19:21:30 +0800

    make another file for methods

--- practice/game.py
+++ practice/game.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import sys
+from gameMethods import moveCircle
 
 #to initialize pygame
 pygame.init()
@@ -15,35 +16,34 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 #to set the title of the window
 pygame.display.set_caption("Practice Game")
 
+# ----------- after this you can any methods you want -------------- just make sure to put them before the game loop
+
 #to make rectangle
 rect = pygame.Rect(100, 100, 50,50) # x, y, width, height
+
+# initial position of circle
+circle_x = 100
+circle_y = 200
+
 
 #to make circle -> pygame.draw.circle(surface, color, center, radius, width=0)
 circle = pygame.draw.circle(screen, (255,0,0), (100,200), 50, 5) # red color
 
-
 play = True
-
 #game loop
 while play:
     
     screen.fill((0, 0, 0))  # Fill the screen with white black color
     
     #to display the rectangle
-    pygame.draw.rect(screen, (255, 0, 0), rect) # Red color
+    #pygame.draw.rect(screen, (255, 0, 0), rect) # Red color
     
-    #pygame.draw.circle(screen, (255,0,0), (100,200), 50, 0)
+    pygame.draw.circle(screen, (255,0,0), (circle_x, circle_y), 30) # red color
     
     #key pressed
     keys = pygame.key.get_pressed()
-    if keys[K_w]:
-        rect.y -= 1  # Move up
-    elif keys[K_s]:
-        rect.y += 1  # Move down
-    elif keys[K_a]:
-        rect.x -= 1  # Move left
-    elif keys[K_d]:
-        rect.x += 1  # Move right
+    # call the move_circle method
+    circle_x, circle_y = moveCircle(keys, circle_x, circle_y)
     
     #to check for events
     for event in pygame.event.get():
---
+++ practice/gameMethods.py
@@ -0,0 +1,22 @@
+import pygame
+from pygame.locals import *
+import sys
+
+# this a file for game methods like moving objects, collision detection, etc.
+# you can add any methods you want here and call them in game.py
+
+
+def moveCircle(keys, circle_x, circle_y):
+    # W -> up
+    # S -> down
+    # A -> left
+    # D -> right
+    if keys[K_w]:
+        circle_y -= 1  # Move up
+    elif keys[K_s]:
+        circle_y += 1  # Move down
+    elif keys[K_a]:
+        circle_x -= 1  # Move left
+    elif keys[K_d]:
+        circle_x += 1  # Move right
+    return circle_x, circle_y
