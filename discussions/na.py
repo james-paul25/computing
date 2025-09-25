a7398e1e4480c0ae6088f46d3439aba48e7c76e9
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-09-26 00:32:40 +0800

    anananana

--- discussions/na.py
+++ discussions/na.py
@@ -1,28 +1,41 @@
 import pygame
 from pygame.locals import *
+from movements import moveCircle
 
 pygame.init()
 
 
 HEIGHT, WIDTH = 600, 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Nov 1 Discussion")  
+pygame.display.set_caption("Sparringggg")  
 
 PURPLE = (128, 0, 128)
+RED = (255, 0, 0)
 
 triangle_points = [(550,50), (600,150), (500, 150)]  # Define the three points of the triangle
 pentagon_points = [(650, 200), (680, 250), (665, 280), (635, 280), (620, 250)]  # Define the five points of the pentagon
+
+circleX, circleY = 80, 100
+
 
 
 running = True
 while running:
     screen.fill((0, 0, 0))  # Fill the screen with black color
     
-    # Draw a triangle
+    # draw triangle
     pygame.draw.polygon(screen, (0, 255, 0), triangle_points) 
     
-    # Draw a pentagon
-    pygame.draw.polygon(screen, PURPLE, pentagon_points) 
+    # draw circle
+    pygame.draw.circle(screen, RED, (circleX, circleY), 20)  
+    
+    keys = pygame.key.get_pressed()
+    
+    # to move circle
+    circleX, circleY = moveCircle(keys, circleX, circleY)
+    
+    # draw a pentagon
+    pygame.draw.polygon(screen, PURPLE, pentagon_points, 2) 
     
     for event in pygame.event.get():
         if event.type == QUIT:
