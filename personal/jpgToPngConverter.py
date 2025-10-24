a4eda63ff8b328465cbbb19c81092b583dab7a82
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-10-25 05:33:37 +0800

    png

--- personal/jpgToPngConverter.py
+++ personal/jpgToPngConverter.py
@@ -1,4 +1,10 @@
 from PIL import Image
 
-original = Image.open("assets/background.jpg")
-original.save("out/background.png")
+def jpg_to_png_converter(jpg_file_path, png_file_path):
+    img = Image.open(jpg_file_path)
+    img.save(png_file_path, 'PNG')
+
+jpg_to_png_converter("assets/ghost.jpg", "out/ghost_converted.png")
+jpg_to_png_converter("assets/bg1.jpg", "out/bg1_converted.png")
+jpg_to_png_converter("assets/bg2.jpg", "out/bg2_converted.png")
+jpg_to_png_converter("assets/bg3.jpg", "out/bg3_converted.png")
